dee9feab6b2204ae87e88b018d4006328cf22e7a
Samanjvd <smnjvd20@gmail.com>
2024-10-31 13:25:57 +0330

    create todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
-import "./App.css";
-import DateType from "./components/datetype/DateType";
-import Cards from "./components/cards/Cards";
-import Gradient from "./components/gradient/Gradient";
-import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [planType, setPlanType] = useState("monthly");
-
-  const monthlyPlans = {
-    price1: "$125",
-    price2: "$225",
-    price3: "$325",
-    date: "/Month",
-  };
-
-  const annuallyPlans = {
-    price1: "$325",
-    price2: "$425",
-    price3: "$525",
-    date: "/Annually",
-  };
-
-  const mPlans = [
-    {
-      type: "basic",
-      price: "10",
-      features: ["a", "b"],
-    },
-  ];
-
-  console.log(monthlyPlans, annuallyPlans);
-
   return (
-    <div className="container">
-      <h1 className="title-pricing">Star Scalling Your Bussines Now</h1>
-      <p className="text-below">
-        Select from best plan,ensuring a perfect match
-      </p>
-      <DateType currentPlan={planType} onChangePlan={setPlanType} />
-      <Cards plans={planType === "monthly" ? monthlyPlans : annuallyPlans} />
-      <Gradient />
+    <div className={styles.container}>
+      <div></div>
     </div>
   );
 }
